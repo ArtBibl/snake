29c28b7c24f6343a4ba80894be54cec68770e165
Artem Biblenko <artbibl84@gmail.com>
2022-01-20 21:49:30 +0200

    snake moving

--- snake.py
+++ snake.py
@@ -1,9 +1,15 @@
 import os
 import time
-
+import random
 import pynput
 
-direction = x
+WIDTH = 0
+HEIGHT = 0
+direction = 0
+directionX = 1
+xfood = 0
+yfood = 0
+snake = [{'x': 1, 'y': 2}, {'x': 2, 'y': 2}]
 
 
 def draw():
@@ -11,16 +17,36 @@ def draw():
 
 
 def move():
-    pass
+    global snake
+    new_head = snake[-1]
+    if direction == 1:
+        new_head['y'] += 1
+    if direction == 2:
+        new_head['y'] -= 1
+    if directionX == 3:
+        new_head['x'] -= 1
+    if directionX == 4:
+        new_head['x'] += 1
+    snake.append(new_head)
+    snake.pop(0)
 
 
 def randfood():
-    pass
+    global xfood, yfood
+    xfood = random.randint(0, WIDTH)
+    yfood = random.randint(0, HEIGHT)
 
 
 def on_press(key):
-    pass
-
+    global direction, directionX
+    if key == pynput.keyboard.Key.up:
+        direction = 1
+    if key == pynput.keyboard.Key.down:
+        direction = 2
+    if key == pynput.keyboard.Key.left:
+        directionX = 3
+    if key == pynput.keyboard.Key.right:
+        directionX = 4
 
 def on_release(key):
     pass
@@ -33,7 +59,8 @@ pynput.keyboard.Listener(
 
 while True:
     os.system('cls')
-    draw()
+#     draw()
+    print(snake)
     move()
-    randfood()
-    time.sleep(0.05)
+    print(snake)
+    time.sleep(1)
