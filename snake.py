d5088a865a46cc1c7ac70d2d315890815580230c
ArtBibl <artbibl84@gmail.com>
2022-01-22 15:51:20 +0200

    Now much better, but random food dosen`t work correct.

--- snake.py
+++ snake.py
@@ -11,32 +11,37 @@ snake = [{'x': 1, 'y': 2},
          {'x': 2, 'y': 2},
          {'x': 3, 'y': 2},
          {'x': 4, 'y': 2}]
-foods = [{'x': 6, 'y': 5}]
+foods = {'x': 6, 'y': 5}
 WIDTH = 30
-HEIGHT = 10
+HEIGHT = 20
+food_not_into_snake = True
 
 
 def draw():
+    global foods
     y = 0
-    while y < HEIGHT:
+    while y < HEIGHT + 1:
         x = 0
-        result = " "
-        while x < WIDTH:
+        result = ""
+        while x < WIDTH + 1:
             char = ' '
             for item in snake:
                 if x == item["x"] and y == item["y"]:
                     char = "0"
-                for food in foods:
-                    if x == food['x'] and y == food['y']:
-                        char = '#'
+                if x == foods['x'] and y == foods['y']:
+                    char = '*'
+                if x == 0 or x == WIDTH:
+                    char = '█'
+                if y == 0 or y == HEIGHT:
+                    char = '█'
             result += char
             x += 1
-        print(" " + result)
+        print("" + result)
         y += 1
 
 
 def move():
-    global snake, xfood, yfood
+    global snake, foods
     new_head = snake[-1].copy()
     if direction == 1:
         new_head['y'] += 1
@@ -46,7 +51,7 @@ def move():
         new_head['x'] -= 1
     if direction == 4:
         new_head['x'] += 1
-    if new_head['x'] == xfood and new_head['y'] == yfood:
+    if new_head['x'] == foods['x'] and new_head['y'] == foods['y']:
         randfood()
     else:
         snake.pop(0)
@@ -54,16 +59,16 @@ def move():
 
 
 def randfood():
-    global foods, xfood, yfood
+    global foods, xfood, yfood, food_not_into_snake
     food_not_into_snake = True
     while food_not_into_snake:
-        xfood = random.randint(0, WIDTH - 1)
-        yfood = random.randint(0, HEIGHT - 1)
-        foods = [{'x': xfood, 'y': yfood}]
+        xfood = random.randint(1, WIDTH - 1)
+        yfood = random.randint(1, HEIGHT - 1)
+        foods = {'x': xfood, 'y': yfood}
+        food_not_into_snake = False
         for item in snake:
-            if item['x'] == xfood and item['y'] == yfood:
-                food_not_into_snake = False
-            else:
+            if item['x'] == foods['x'] and item['y'] == foods['y']:
+                food_not_into_snake = True
                 return foods
 
 
